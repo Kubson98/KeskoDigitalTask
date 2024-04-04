8cedaf0597ba850e2e27ce98dec74dc04716f39c
Jakub Sedal <sedal.jakub@gmail.com>
2024-04-04 14:53:42 +0200

    Initial Commit

---
+++ KeskoSenukaiDigitalTask/KeskoSenukaiDigitalTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeskoSenukaiDigitalTaskApp.swift
+//  KeskoSenukaiDigitalTask
+//
+//  Created by Jakub Sędal on 04/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct KeskoSenukaiDigitalTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
